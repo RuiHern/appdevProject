d65bff909fa7e4e8b8250e74f70784bc10346d42
bobbybobber <140957907+bobbybobber@users.noreply.github.com>
2024-01-02 17:02:09 +0800

    updated css

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from Forms import CreateUserForm, CreateCustomerForm
 import shelve, User, Customer
 
-app = Flask(__name__,static_url_path='/CSS')
+app = Flask(__name__,static_url_path='/Static')
 
 @app.route('/')
 def home():
