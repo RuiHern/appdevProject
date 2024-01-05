7027c3340948e2eff00fb8fd8b7b9c77f53964f0
bobbybobber <140957907+bobbybobber@users.noreply.github.com>
2024-01-05 10:09:01 +0800

    creating "create user page" + validation of password and email

--- Forms.py
+++ Forms.py
@@ -1,12 +1,11 @@
 from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators
-from wtforms.fields import EmailField, DateField
+from wtforms.fields import EmailField, DateField, PasswordField
 
 class CreateUserForm(Form):
     first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
     last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
-    remarks = TextAreaField('Remarks', [validators.Optional()])
+    email = EmailField('Email', [validators.Email(), validators.DataRequired()])
+    password = PasswordField('Password',[validators.length(min=5,max=15),validators.data_required()])
 
 class CreateCustomerForm(Form):
     first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
