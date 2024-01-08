92b8c9f64c3896babdccfbe3ac46d43a57522758
yjcha <yjchang11062006@gmail.com>
2024-01-08 17:03:27 +0800

    thanks

--- Forms.py
+++ Forms.py
@@ -20,3 +20,21 @@ class CreateCustomerForm(Form):
 class logininformation(Form):
     email = EmailField('Email', [validators.Email(), validators.DataRequired()])
     password = PasswordField('Password', [validators.length(min=5, max=15), validators.data_required()])
+
+
+class createProject(Form):
+    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+    phone = StringField('Phone', [validators.Length(min = 8), validators.DataRequired()])
+    house_type = RadioField('House Type', choices=[('AP','Appartment'), ('BUN', 'Bungalow'), ('HDB2', '2-Room HDB'),
+                                                   ('HDB3', '3-Room HDB'),('HDB4', '4-Room HDB'),('HDB5', '5-Room HDB')
+                                                   ,('CON', 'Condominium'), validators.DataRequired()])
+    address = StringField('House address', [validators.Length(min=1, max=150), validators.DataRequired()])
+    postal_code = StringField('Postal', [validators.Length(min=1, max=150), validators.DataRequired()])
+
+
+    house_theme = RadioField('House Type', choices=[('AP', 'Appartment'), ('BUN', 'Bungalow'), ('HDB2', '2-Room HDB'),
+                                                   ('HDB3', '3-Room HDB'), ('HDB4', '4-Room HDB'),
+                                                   ('HDB5', '5-Room HDB')
+        , ('CON', 'Condominium'), validators.DataRequired()])
+    add_comment = TextAreaField('Remarks', [validators.Optional()])
